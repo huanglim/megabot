8811c9ade9ed8b705f837b49d5cdebe30c653399
huanglmw <[huanglmw@cn.ibm.com]>
2018-07-05 16:27:45 +0800

    enhance the html layout

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,17 @@
-import os,time,json, logging
-from flask import render_template,request,jsonify,send_from_directory,\
-    current_app,session,redirect,g, abort, url_for, flash
+import os, time, json, logging
+from flask import render_template, request, jsonify, send_from_directory, \
+    current_app, session, redirect, g, abort, url_for, flash
 from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
 from . import main
-from .. import watson_conversion,cloudant_nosql_db
+from .. import watson_conversion, cloudant_nosql_db
 from ..auth.views import login
 from .forms import RegistrationForm
 from wtforms import SelectField
 from threading import current_thread
-from ..utility import read_excel,  \
-                        verify_select_level, verify_email_format, \
-                        verify_template, verify_input,  \
-                        get_approver, convert_country, convert_company
+from ..utility import read_excel, \
+    verify_select_level, verify_email_format, \
+    verify_template, verify_input, \
+    get_approver, convert_country, convert_company
 
 from werkzeug.utils import secure_filename
 from json2html import *
@@ -20,7 +20,8 @@ from pprint import pprint
 
 logging.basicConfig(level=logging.INFO)
 
-@main.route('/',methods=['GET','POST'])
+
+@main.route('/', methods=['GET', 'POST'])
 @login
 def index():
     logging.info('in index')
@@ -40,6 +41,7 @@ def index():
     #         return render_template('index.html', name=session['id_token']['firstName'].replace('%20', ' '))
     return render_template('index.html', name=session['id_token']['firstName'].replace('%20', ' '))
 
+
 @main.route('/oidc_callback')
 def oidc_callback():
     logging.info('in callback')
@@ -50,14 +52,14 @@ def oidc_callback():
             credentials = current_app.flow.step2_exchange(code)
             id_token = credentials.id_token
             logging.info('id token is {}'.format(id_token))
-            id_token['firstName'] = id_token['firstName'].replace('%20',' ')
+            id_token['firstName'] = id_token['firstName'].replace('%20', ' ')
             id_token['lastName'] = id_token['lastName'].replace('%20', ' ')
             session['id_token'] = id_token
             # response = current_app.make_response\
             #     (render_template('index.html', name=session['id_token']['firstName']))
-            response = current_app.make_response\
+            response = current_app.make_response \
                 (redirect(current_app.config['next']))
-            response.set_cookie('id_token',id_token)
+            response.set_cookie('id_token', id_token)
         except FlowExchangeError:
             abort(401)
         finally:
@@ -66,6 +68,7 @@ def oidc_callback():
     else:
         return redirect(current_app.config['next'])
         # return render_template('index.html', name=session['id_token']['firstName'].replace('%20', ' '))
+
 
 @main.route('/userinfo', methods=['GET'])
 def userinfo():
@@ -80,9 +83,9 @@ def userinfo():
         abort(404)
     return render_template('userinfo.html', user=user)
 
-@main.route('/edit_access', methods=['GET','POST'])
-def edit_access():
 
+@main.route('/edit_access', methods=['GET', 'POST'])
+def edit_access():
     if request.method == 'POST':
         _country = request.values.getlist('country')
         country_access = list(map(convert_country, _country))
@@ -100,9 +103,9 @@ def edit_access():
         doc = cloudant_nosql_db.get_user_info(session['id_token']['emailAddress'])
 
         # Send the email for approval
-        to =  get_approver(session['id_token']['emailAddress'])
+        to = get_approver(session['id_token']['emailAddress'])
         subject = 'Access request for {}'.format(doc.get('emailAddress'))
-        confirm_link = current_app.config['HOME_URL']+'confirm_access/'+session['id_token']['emailAddress']
+        confirm_link = current_app.config['HOME_URL'] + 'confirm_access/' + session['id_token']['emailAddress']
         sender = current_app.config['MAIL_SENDER']
         # send_email(approver, subject, 'mail_confirm_access', confirm_link)
         cloudant_nosql_db.write_to_mail(to, sender, subject, confirm_link, doc.get('emailAddress'))
@@ -111,10 +114,10 @@ def edit_access():
 
     return render_template('edit_access.html')
 
-@main.route('/confirm_access/<emailAddress>', methods=['GET','POST'])
+
+@main.route('/confirm_access/<emailAddress>', methods=['GET', 'POST'])
 @login
 def confirm_access(emailAddress):
-
     # emailAddress = request.args.get('emailAddress')
     if not emailAddress:
         abort(404)
@@ -140,7 +143,7 @@ def confirm_access(emailAddress):
                 raise
             else:
                 cloudant_nosql_db.update_pending_country_accesses(requester.get('_id'),
-                                                                   None)
+                                                                  None)
         if requester.get('pending_company_accesses'):
             try:
                 cloudant_nosql_db.update_approved_company_accesses(requester.get('_id'),
@@ -149,7 +152,7 @@ def confirm_access(emailAddress):
                 raise
             else:
                 cloudant_nosql_db.update_pending_company_accesses(requester.get('_id'),
-                                                                   None)
+                                                                  None)
 
         if 'https://' not in request.url:
             _url = request.url.replace('http://', 'https://')
@@ -160,15 +163,16 @@ def confirm_access(emailAddress):
         # return render_template('confirm_access.html', user=user)
     return render_template('confirm_access.html', approver=approver, requester=requester)
 
-@main.route("/ask",methods=['POST'])
+
+@main.route("/ask", methods=['POST'])
 def ask():
     input = str(request.form['messageText'])
     return jsonify({'status': 'OK',
                     'answer': watson_conversion.get_response(input)})
 
-@main.route("/enable_schedule",methods=['GET'])
-def enable_schedule():
 
+@main.route("/enable_schedule", methods=['GET'])
+def enable_schedule():
     _id = request.args['id']
     if not _id:
         render_template('404')
@@ -181,9 +185,9 @@ def enable_schedule():
     else:
         return jsonify({'status': True})
 
-@main.route("/disable_schedule",methods=['GET'])
-def disable_schedule():
 
+@main.route("/disable_schedule", methods=['GET'])
+def disable_schedule():
     _id = request.args['id']
     if not _id:
         render_template('404')
@@ -196,6 +200,7 @@ def disable_schedule():
     else:
         return jsonify({'status': True})
 
+
 @main.route("/schedule", methods=["GET", "POST"])
 @login
 def schedule():
@@ -206,6 +211,7 @@ def schedule():
         logging.info('There is no schedule for the user {}'.format(emailAddress))
         schedules = None
     return render_template('schedule.html', schedules=schedules)
+
 
 @main.route("/task", methods=["GET", "POST"])
 @login
@@ -219,7 +225,8 @@ def task():
         tasks = None
     return render_template('task.html', tasks=tasks)
 
-@main.route("/edit_schedule", methods=["GET","POST"])
+
+@main.route("/edit_schedule", methods=["GET", "POST"])
 def edit_schedule():
     form = RegistrationForm(request.form)
     # user = cloudant_nosql_db.get_user_info(session['id_token']['emailAddress'])
@@ -243,14 +250,14 @@ def edit_schedule():
             "Select Report Criteria": form.sel_rpt_crit.data,
             "Select Report Format": form.sel_rpt_format.data,
             "Input Field": form.input_field.data,
-            "run date":str(form.run_date.data),
-            "schedule cycle":form.schedule_cycle.data
+            "run date": str(form.run_date.data),
+            "schedule cycle": form.schedule_cycle.data
         }
         cloudant_nosql_db.write_to_schedule(schedule_record)
         redirect_url = url_for('main.schedule')
         if 'https://' not in redirect_url:
             # to fix redirect issue for bluemix
-            _url = current_app.config['HOME_URL'][:-1]+redirect_url
+            _url = current_app.config['HOME_URL'][:-1] + redirect_url
         else:
             _url = redirect_url
         logging.info('The redirect url is {}, the old one is {}'.format(_url, redirect_url))
@@ -260,17 +267,19 @@ def edit_schedule():
 
     return render_template('edit_schedule.html', form=form, user=user)
 
+
 # download template file
 @main.route('/download/<filename>')
 def download_template(filename):
-    logging.debug('In the function: {}, the file is {}'.\
-                  format('download_template',current_app.config['DOWNLOAD_FOLDER']))
+    logging.debug('In the function: {}, the file is {}'. \
+                  format('download_template', current_app.config['DOWNLOAD_FOLDER']))
     if os.path.isfile(os.path.join(current_app.config['DOWNLOAD_FOLDER'],
                                    filename)):
         logging.debug('Has the file{} '.format(os.path.join(current_app.config['DOWNLOAD_FOLDER'],
-                                   filename)))
+                                                            filename)))
         return send_from_directory(current_app.config['DOWNLOAD_FOLDER'],
                                    filename, as_attachment=True)
+
 
 # upload parameters file
 @main.route('/upload', methods=['POST'], strict_slashes=False)
@@ -287,31 +296,31 @@ def upload():
     # get the file object from request
     f = request.files['file']
     # check if allowed file format
-    allowed_file = lambda x : '.' in x and x.rsplit('.',1)[1] \
-                              in current_app.config['ALLOWED_EXTENSIONS']
+    allowed_file = lambda x: '.' in x and x.rsplit('.', 1)[1] \
+                             in current_app.config['ALLOWED_EXTENSIONS']
 
     if f and allowed_file(f.filename):
         fname = secure_filename(f.filename)
         # get the filename and file extension
-        filename,ext = fname.rsplit('.', 1)
-        seconds = str(time.time()).replace('.','_')
+        filename, ext = fname.rsplit('.', 1)
+        seconds = str(time.time()).replace('.', '_')
         # renamed file name with adding time(the seconds)
         new_filename = filename + '_' + seconds + '.' + ext
         # save file to upload folder
         f.save(os.path.join(file_dir, new_filename))
         full_file_name = os.path.join(file_dir, new_filename)
-        #read template
-        logging.info('complete save %s' %full_file_name)
+        # read template
+        logging.info('complete save %s' % full_file_name)
 
         try:
             excel_content = read_excel(full_file_name)
         except Exception as e:
-            logging.error('Error in read execl! %s' %e)
+            logging.error('Error in read execl! %s' % e)
             status = False
             status_message = 'Please upload latest version of template and upload the correct one." \
             "Your report is not running. '
 
-        logging.debug('The email address is %s, type is %s' %(user_addr, type(user_addr)))
+        logging.debug('The email address is %s, type is %s' % (user_addr, type(user_addr)))
 
         if not verify_template(excel_content):
             status = False
@@ -323,12 +332,12 @@ def upload():
 
         if status:
             for index, request_record in enumerate(excel_content):
-                logging.info('loop & verify request record %s' %request_record)
+                logging.info('loop & verify request record %s' % request_record)
                 if not verify_select_level(request_record.get('Select Report Level'),
-                    request_record.get('Select Country/Company')):
+                                           request_record.get('Select Country/Company')):
                     status = False
                     status_message = "Invalid report level or country/company level, " \
-                    "Please double check your input, Your input is: "
+                                     "Please double check your input, Your input is: "
                     break
                 # elif not verify_date(request_record['Weekending Date Range Start date'],
                 #                      request_record['Weekending Date Range End date']):
@@ -341,20 +350,20 @@ def upload():
                                                          request_record['Select Country/Company']):
                     status = False
                     status_message = "Your privilege checking is failed. Please go to userinfo to confirm your access. " \
-                    "Userinfo is in the right-top of screen."
+                                     "Userinfo is in the right-top of screen."
                     return jsonify({'status': 'failed',
-                        'request_status':status_message
-                        })
+                                    'request_status': status_message
+                                    })
                 elif not verify_input(request_record['Input Field']):
                     # logging.debug(request_record['Input Field'])
                     status = False
-                    status_message = "Invalid Input Field, please double check your input,"\
-                     "use COMMA to seprate your account or sn and ensure there is " \
-                     "no Enter in your input. \nYour input is: "
+                    status_message = "Invalid Input Field, please double check your input," \
+                                     "use COMMA to seprate your account or sn and ensure there is " \
+                                     "no Enter in your input. \nYour input is: "
                     break
                 else:
                     status_message = "Your {} record is now in process, the email will send to {}," \
-                                     "please wait for a while!".format(index+1, user_addr)
+                                     "please wait for a while!".format(index + 1, user_addr)
                     cloudant_nosql_db.write_to_request(request_record, user=user_addr, status='submitted')
 
         # convert excel to html table
@@ -368,9 +377,9 @@ def upload():
         #     # which can be stored in session or g
         #     cloudant_nosql_db.write_to_db(document,user=None)
 
-        return jsonify({'status': 'OK', 'excelHTML':excel_html,
-                        'filename':new_filename,
-                        'request_status':status_message
+        return jsonify({'status': 'OK', 'excelHTML': excel_html,
+                        'filename': new_filename,
+                        'request_status': status_message
                         })
     else:
-        return jsonify({'status':'Not OK'})
+        return jsonify({'status': 'Not OK'})
--- megabot.py
+++ megabot.py
@@ -1,14 +1,21 @@
 import os
 from app import create_app
 
+# Init the flask app with the config setting
+# Checking the environment, if it is cloud environment, then use production setting
+# Otherwise use default setting
 if os.getenv('VCAP_APP_HOST'):
     app = create_app(os.getenv('MEGABOT_CONFIG', 'production'))
 else:
-    app = create_app(os.getenv('MEGABOT_CONFIG','default'))
+    app = create_app(os.getenv('MEGABOT_CONFIG', 'default'))
 
 if __name__ == '__main__':
+
+    # Get the PORT & HOST
     PORT = int(os.getenv('VCAP_APP_PORT', '8080'))
     HOST = str(os.getenv('VCAP_APP_HOST', 'localhost'))
+
+    # Run the Flask server
     if app.config.get('PRODUCTION'):
         app.run(host=HOST, port=PORT)
     else:
